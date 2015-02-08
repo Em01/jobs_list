306393a49ba7f8c4b279c77c24b8ca422f7e0377
Em01 <emma_williams1@hotmail.co.uk>
2015-02-08 23:05:33 +0000

    more work on single dependency logic

--- lib/jobs_list.rb
+++ lib/jobs_list.rb
@@ -2,9 +2,12 @@ class JobsList
 
 	def self.sequence(jobs={})
 		grouped_jobs = jobs.group_by{|k, v| v}
-
 		grouped_jobs.map do |k,v|
-			[k] + v[1]
+			array = []
+			if k 
+				array = array + [k]
+			end
+			array + v.map{|a| a[1]}
 		end.flatten
 	end
 end 
