a353310b59fed4d75a573e7ed08b66601cfdbd56
Em01 <emma_williams1@hotmail.co.uk>
2015-02-08 22:52:20 +0000

    working through the single dependency still

--- lib/jobs_list.rb
+++ lib/jobs_list.rb
@@ -1,8 +1,11 @@
 class JobsList
 
 	def self.sequence(jobs={})
-		jobs.map do |key,value|
-			key
-		end
-	end	
+		parent_jobs = jobs.values
+		grouped_jobs = jobs.group_by{|k, v| v}
+
+		grouped_jobs.map do |k,v|
+			[k] + v.keys
+		end.flatten
+	end
 end 
